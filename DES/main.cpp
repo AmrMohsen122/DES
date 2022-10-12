6b9609690b2d340ade2605138c2415d65db6f1dc
Amr Mohsen <amrmohsen2000@hotmail.com>
2022-10-12 19:34:06 +0200

    added generated key function

--- DES/main.cpp
+++ DES/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
-
-#define COMMON_MACROS_H
+#include <bitset>
+/******************************************************* COMMON MACROS ********************************************************************/
 #define GET_BIT(VAR,BIT_NUMBER) (((VAR) & ((uint64_t)1 << (BIT_NUMBER))) >> (BIT_NUMBER))
 #define INSERT_BIT(VAR,BIT_VAL,BIT_NUMBER) (VAR = ((VAR & ~((uint64_t)1 << BIT_NUMBER)) | ((uint64_t)(BIT_VAL) << BIT_NUMBER)))
-#define LEFT_CIRCULAR_SHIFT(VAR,SHIFT_NUMBER) ((VAR) = ((VAR) << (SHIFT_NUMBER)) | ((VAR) >> ((sizeof(VAR) * 8) - (SHIFT_NUMBER))))
+/*
+THIS MACRO IS NOT GENERIC AND ONLY INTENDED FOR THE FOLLOWING USECASE
+LEFT CIRCULAR SHIFT OF 32-BIT VARIABLE THAT IS USED TO CARRY A 28-BIT NUMBER
+USE WITH CARE
+*/
+#define LEFT_CIRCULAR_SHIFT(VAR,SHIFT_NUMBER) ((VAR) = (((VAR) << (SHIFT_NUMBER)) & 0x0FFFFFFF)| ((VAR) >> (28 - (SHIFT_NUMBER))))
 #define SWAP_32_BITS(VAR) ((VAR) = ((VAR) << 32) | ((VAR) >> 32))
 #define BIT_IS_SET(VAR, BIT_NUMBER) ((VAR) & ( 1ULL << (BIT_NUMBER ) ) )
 #define SET_BIT(VAR , BIT_NUMBER) ((VAR) |= (1ULL << (BIT_NUMBER)))
+/*****************************************************************************************************************************************/
 
+/******************************************************* DEFINITIONS ********************************************************************/
 #define PER_CHOICE_1_SIZE		56
 #define PER_CHOICE_2_SIZE		48
 #define PER_INITIAL_SIZE		64
 #define PER_INVERSE_SIZE		64
 #define DES_EXPANSION_SIZE		48
 #define F_PERMUTATION_SIZE		32
+/****************************************************************************************************************************************/
+
+/******************************************************* ENUMS **************************************************************************/
 
 enum word_size {
 	_64_BIT_WORD , _56_BIT_WORD
 };
 
+/****************************************************************************************************************************************/
 
+/******************************************************* GLOBAL VARIABLES ***************************************************************/
 uint8_t init_permumtation_table[PER_INITIAL_SIZE] = {
 	58, 50, 42, 34, 26, 18, 10, 2,
 	60, 52, 44, 36, 28, 20, 12, 4,
@@ -74,28 +86,42 @@ uint8_t	F_permutation[F_PERMUTATION_SIZE] = {
 	 2,  8, 24, 14, 32, 27,  3,  9,
 	19, 13, 30,  6, 22, 11,  4, 25
 };
+/****************************************************************************************************************************************/
 
+/******************************************************* FUNCTION PROTOTYPES *************************************************************/
 uint64_t permute_1D(const uint8_t permutation_table[], uint64_t variable , uint8_t size);
 
 void split_word(uint64_t word, uint32_t& l_half_word, uint32_t& r_half_word, enum word_size);
 
 void generate_keys(uint64_t original_key , uint64_t key_container[]);
+/****************************************************************************************************************************************/
 
+
+/******************************************************* MAIN *************************************************************/
 int main(void) {
 	/*MSB----LSB*/
-	uint64_t message = 0x7123456789ABCDEF;
-	//uint64_t key = 0x133457799BBCDFF1;
-	//uint64_t result = permute_1D(permutation_choice_1, message, PER_CHOICE_1_SIZE);
-	std::cout << std::hex << message <<std::endl;
-
+	uint64_t key_container[16] = {0};
+	//uint64_t message = 0x7123456789ABCDEF;
+	uint64_t key = 0x8FFB3DD99EEA2CC8;
+	generate_keys(key, key_container);
+	//for (int i = 0; i < 64; i++) {
+	//	std::cout << GET_BIT(key, i);
+	//}
+	//std::cout << std::bitset<64>(key) << std::endl;
+	//key = permute_1D(permutation_choice_1, key, PER_CHOICE_1_SIZE);
+	//std::cout << std::bitset<PER_CHOICE_1_SIZE>(key) << std::endl;
+	for (uint64_t k : key_container) {
+		//std::cout <<std::bitset<64>(k) << std::endl;
+		std::cout <<std::hex << (k) << std::endl;
+	}
 	return 0;
 }
 
+/****************************************************************************************************************************************/
 
 
 
-
-
+/******************************************************* FUNCTION DEFINITIONS *************************************************************/
 
 uint64_t permute_1D(const uint8_t permutation_table[], uint64_t variable , uint8_t size) {
 	uint64_t result = 0;
@@ -124,12 +150,20 @@ void split_word(uint64_t word, uint32_t& l_half_word, uint32_t& r_half_word, enu
 
 }
 
-void generate_keys(uint64_t original_key, const uint64_t key_container[]) {
+void generate_keys(uint64_t original_key, uint64_t key_container[]) {
 	uint32_t key_upper_half = 0;
 	uint32_t key_lower_half = 0;
+	uint64_t current_key = 0;
+	const uint8_t shifts[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+	original_key = permute_1D(permutation_choice_1, original_key, PER_CHOICE_1_SIZE);
 	split_word(original_key, key_upper_half, key_lower_half , _56_BIT_WORD);
 	for (int i = 0; i < 16; i++) {
-		
-	
+		LEFT_CIRCULAR_SHIFT(key_upper_half , shifts[i]);
+		LEFT_CIRCULAR_SHIFT(key_lower_half , shifts[i]);
+		/*concatenate the two parts of key for the next permutation*/
+		current_key = (static_cast<uint64_t>(key_upper_half) << 28) | (key_lower_half);
+		current_key = permute_1D(permutation_choice_2, current_key, PER_CHOICE_2_SIZE);
+		key_container[i] = current_key;
 	}
 }
+/****************************************************************************************************************************************/
